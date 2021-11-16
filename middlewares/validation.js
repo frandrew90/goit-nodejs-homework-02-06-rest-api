db86b825e8f0fc3b2d946ddc18a5f360a1a3ed0d
frandrew90 <frandrewjs@gmail.com>
2021-11-16 21:07:39 +0200

    fixed validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -6,7 +6,7 @@ const joi = require("joi");
 
 const validation = (req, res, next) => {
   const validationSchema = joi.object({
-    name: joi.string().alphanum().min(3).max(30).required(),
+    name: joi.string().min(3).max(30).required(),
     email: joi.string().email({ minDomainSegments: 2 }).required(),
     phone: joi.string().min(10).max(25).required(),
   });
